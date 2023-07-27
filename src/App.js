5def8eb34f6df4112fddef823706812f007a9cce
Mo. Ivan <moivanismail@gmail.com>
2023-07-27 14:20:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import {config} from './Config';
+import { PublicClientApplication } from '@azure/msal-browser';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      user: {},
+      error: null
+    };
+this.login = this.login.bind(this);
+// initiate the MSAL application object
+this.publicClientApplication = new PublicClientApplication({
+  auth: {
+    clientId: config.appId,
+    redirectUri: config.redirectUri,
+    authority: config.authority,
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true,
+  }
+});
+  }
+async login() {
+  try{
+    // login via popup
+    await this.publicClientApplication.loginPopup(
+      {
+        scopes: config.scopes,
+        prompt: "select_account"
+      });
+      this.setState({isAuthenticated: true});
+  }
+  catch(err){
+    this.setState({
+      isAuthenticated: false,
+      user: {},
+      error: err
+    });
+  }
 }
 
+logout(){
+  this.publicClientApplication.logout();
+}
+
+render(){
+  return(
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        {this.state.isAuthenticated ? <p>
+          Successful Logged in
+        </p>:
+        <p>
+          <button onClick={() => this.login()}>Login</button>
+        </p>
+        }
+      </header>
+      </div>
+    );
+  }
+}
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: '59538ae0-0806-43cd-906b-0ff7029afff1',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read',
+    ],
+    authority: 'https://login.microsoftonline.com/common',
+}
